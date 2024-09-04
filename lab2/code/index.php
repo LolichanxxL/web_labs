8d044d96c0be01c1c2f410ccabf7e587e16f8421
LolichanxxL <paulbobnev@yandex.ru>
2024-09-04 22:00:15 +0200

    [lab2] Task 1 added

--- lab2/code/index.php
+++ lab2/code/index.php
@@ -1,2 +1,4 @@
 <?php
-echo phpinfo();
+
+require 'task1.php';
+
---
+++ lab2/code/task1.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Task 1) ";
+
+$very_bad_unclear_name = "15 chicken wings";
+
+$order = &$very_bad_unclear_name;
+
+$order .= " and some coke";
+
+echo "\nYour order is: $very_bad_unclear_name.";
+
+
+
