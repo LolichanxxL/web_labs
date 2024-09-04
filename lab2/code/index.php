951036c9ac4cb5f10b64b4870165395d408c5da6
LolichanxxL <paulbobnev@yandex.ru>
2024-09-04 22:36:12 +0200

    [lab2] Task 3 added

--- lab2/code/index.php
+++ lab2/code/index.php
@@ -2,4 +2,5 @@
 header('Content-type: text/plain');
 require 'task1.php';
 require 'task2.php';
+require 'task3.php';
 
---
+++ lab2/code/task3.php
@@ -0,0 +1,9 @@
+<?php
+echo "\nTask 3)\n";
+
+$num_languages = 4;
+$months = 11;
+$days = $months * 16;
+$days_per_language = $days / $num_languages;
+
+echo $days_per_language . "\n";
